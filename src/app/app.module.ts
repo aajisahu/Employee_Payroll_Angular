2996e84bd5cec7ece3e3999ba4181b3f90901367
aajisahu <aajisahu@gmail.com>
2022-01-01 01:40:27 +0530

    Added: Employeepayroll Frontend with dash-board design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashBoardComponent,
+   
+
   ],
   imports: [
     BrowserModule
