cd1f535575aca89d69a3cd3ae84220659feef9bf
aajisahu <aajisahu@gmail.com>
2022-01-08 12:19:12 +0530

    Added: Sending Data From Front End (Post Method) Storing into DataBase

--- src/app/add-employee/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee/add-employee.component.ts
@@ -1,10 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { EmployeeService } from 'src/app/service/employee.service';
 import { PostService } from 'src/app/service/post-service';
-
-
-
+import { EmpDetails } from 'src/app/emp-details';
+import { FormControl, FormGroup, FormBuilder, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-employee',
@@ -13,19 +11,67 @@ import { PostService } from 'src/app/service/post-service';
 })
 export class AddEmployeeComponent implements OnInit {
 
-  constructor(private employee: EmployeeService ){}
-
-  addEmp:PostService[]=[];
-  ngOnInit(): void {
-  }
-  addNewEmp(addEmpForm:any){
-    console.log(JSON.stringify(addEmpForm.value));
-    this.addEmp=addEmpForm.value;
-    this.employee.createEmp(this.addEmp).subscribe(data =>{
-    
-    })
-  }
+  Day=["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"];
+  Month=["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+  Year=["2016","2017","2018","2019","2020","2021","2022"];
+  Dept=["HR","Finance","Engineer","Salse","Other"];
+  Salary=[10000,15000,20000,25000,30000,35000,40000];
   
+  name: String="";
+  gender: String="";
+  department:any="";
+  salary: String="";
+  day:String="";
+  month:String="";
+  year:String="";
+  note: String="";
+  profilePic:String="";
+
+  signupForm: any;
+  date: any;
+  m: any;
+  y: any;
+
+  constructor(private formBuilder:FormBuilder, private employeeService:EmployeeService  ){
+    this.signupForm=formBuilder.group({
+      name:new FormControl(),
+      gender: new FormControl(),
+      department:new FormControl(),
+      salary:new FormControl(),
+      day:new FormControl(),
+      month:new FormControl(),
+      year:new FormControl(),
+      note:new FormControl(),
+      profilePic:new FormControl(),
+    });  
+  }
+  startDate=`${this.day} ${this.month} ${this.year}`;
     
+  ngOnInit() {
+  }
+
+  PostData(signupForm : any){
+    console.log(signupForm.controls);
+    console.log(this.startDate);
+  }
+
+  addNewEmployee(){
+    const newformData={
+      name: this.signupForm.controls.name.value,
+      gender:this.signupForm.controls.gender.value,
+      department:this.signupForm.controls.department.value, 
+      salary: this.signupForm.controls.salary.value,
+      startDate: this.signupForm.controls.day.value+" "+this.signupForm.controls.month.value+" "+this.signupForm.controls.year.value,
+      note :this.signupForm.controls.note.value,
+      profilePic:this.signupForm.controls.profilePic.value
+  }
+  this.employeeService.createContact(newformData).subscribe((data: any) =>
+    console.log(JSON.stringify(data))
+    );
+  }
+
 
 }
+/**
+ * {"message":"Create Employee PayrollData:","employeePayrollDataList":null,"data":{"employeeId":13,"name":"Ashish","salary":40000,"gender":"Male","startDate":"2022-02-02","note":"Front End Data","profilePic":"../assets/profile-images/Ellipse -3.png","departments":null}}
+ */
